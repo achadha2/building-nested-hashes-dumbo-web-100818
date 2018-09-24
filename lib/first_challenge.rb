7790d652ee25ce168dba0129c874e7a4e4cbf1a6
andy.chadha2 <andy.chadha2@gmail.com>
2018-09-24 18:40:15 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -9,7 +9,21 @@ def first_challenge
           :name => "Lady Montague"
           :age => "54"
         }
-       
+       :hero => {
+         :name => "Romeo"
+         :age => "15"
+       }
+       :hero_friends => [{
+         :name => "Benvolio", 
+         :age => "17"
+         :attitude => "worried"
+       },
+       { :name => "Mercutio"
+         :age => "18"
+         :attitude => "hot-headed"
+       }
+      ]
+       }
      }
    }
 end
